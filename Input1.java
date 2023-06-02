6b07904645b20040d8b9fceae7f71e4458ab0975
bhargavikusi <131340042+bhargavikusi@users.noreply.github.com>
2023-06-02 17:22:10 +0530

    Add files via upload

---
+++ Input1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Input2{
+public static int add(int a,int b)
+{
+int c=a+b;
+return c;
+}
+public static void main(
+Scanner scob=new Scanner(System.in);
+System.out.println("Enter a Number:");
+int num1;
+num1=scob.nextInt();
+System.out.println("Enter a Number:");
+int num2;
+num2=scob.nextInt();
+System.out.println("num1="+num1+","+"num2="+num2);
+}
+}
