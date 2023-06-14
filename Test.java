1bd151b77186b8ee8ff8a6ed8acd6e382341d297
bhargavikusi <131340042+bhargavikusi@users.noreply.github.com>
2023-06-14 14:07:52 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,19 +1,21 @@
-import java.util.Scanner;
-class PolymorphismExample1{
-void print(){
-System.out.println("Welcome ");
+class ChildThread extends Thread{
+public void run(){
+for(int i=1;i<=4;i++){
+try{
+Thread.sleep(500);
 }
-void print(String n){
-System.out.println("Welcome "+n);
+catch(Exception e){
+System.out.println(e);
+}
+System.out.println("child thread execution-"+i);
+}
 }
 }
 class Test{
-public static void main(String args[]){
-PolymorphismExample1 ob1=new PolymorphismExample1();
-ob1.print();
-Scanner sc=new Scanner(System.in);
-System.out.println("Please type your name:");
-String s=sc.next();
-ob1.print(s);
+public static void main(String args[])throws Exception{
+ChildThread th1=new ChildThread();
+th1.start();
+th1.join();
+System.out.println("main thread completed");
 }
 }
